490ae15c318bcfd49d2b6b073c895baf6b2e2501
marcellozzetti <48164092+marcellozzetti@users.noreply.github.com>
2022-07-14 01:50:35 -0300

    spring-boot setup

---
+++ src/main/java/com/devops/lab/rest/Greeting.java
@@ -0,0 +1,23 @@
+package com.devops.lab.rest;
+
+/**
+ * Created by marcello.ozzetti on 14/07/22.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
