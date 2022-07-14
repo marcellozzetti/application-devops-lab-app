8d2ca42822e9bd854de582224322c6483dda544f
marcellozzetti <48164092+marcellozzetti@users.noreply.github.com>
2022-07-14 00:30:22 -0300

    init

---
+++ src/test/java/com/devops/lab/LabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
